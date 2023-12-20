9ed8328669929754766753aa69cb193230ba0cc8
Jonathan Jouty <me@jonathanjouty.com>
2023-12-20 09:09:17 +0100

    finish tests with law tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,12 +1,16 @@
 module Main (main) where
 
 import Test.Tasty
-import Test.Tasty.QuickCheck as QC
 import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck as QC
+import Test.QuickCheck.Gen as Gen
+import Test.Tasty.QuickCheck.Laws
 
 import Data.Aeson
-import GHC.Generics (Generic)
+import Data.Typeable
 import Data.Aeson.Possible
+import Data.Monoid (Sum)
+import GHC.Generics (Generic)
 
 main :: IO ()
 main = defaultMain tests
@@ -14,14 +18,41 @@ main = defaultMain tests
 tests :: TestTree
 tests = testGroup "Tests" [laws, unitTests]
 
+newtype A_Possible a = A_Possible { unwrap :: Possible a }
+  deriving stock (Show, Generic, Functor)
+  deriving newtype (Eq, Semigroup, Monoid, Applicative)
+
+instance Arbitrary a => Arbitrary (A_Possible a) where
+  arbitrary = A_Possible <$> Gen.oneof
+    [pure Missing, pure HaveNull, HaveData <$> arbitrary]
+  shrink (A_Possible v) = A_Possible <$> genericShrink v
+
 laws :: TestTree
-laws = testGroup "Laws" [] -- TODO
+laws = testGroup "Laws"
+  [ testEqLaws (Proxy :: Proxy (A_Possible Bool))
+  , testMonoidLaws (Proxy :: Proxy (A_Possible (Sum Int)))
+  , testSemigroupLaws (Proxy :: Proxy (A_Possible (Sum Int)))
+  , testFunctorLaws
+      (Proxy :: Proxy (A_Possible))
+      (Proxy :: Proxy Int)
+      (Proxy :: Proxy Int)
+      (Proxy :: Proxy Int)
+      (Proxy :: Proxy Int)
+      (\_ fu1 fu2 -> fu1 == fu2)
+  , testApplicativeLaws
+      (Proxy :: Proxy (A_Possible))
+      (Proxy :: Proxy Int)
+      (Proxy :: Proxy Int)
+      (Proxy :: Proxy Int)
+      (Proxy :: Proxy Int)
+      (\_ fu1 fu2 -> fu1 == fu2)
+  ]
 
 data TestData = TestData
   { a :: Possible Bool
   , b :: Possible Bool
   , c :: Possible Bool
-  } deriving (Show, Generic, Eq)
+  } deriving stock (Show, Generic, Eq)
 
 instance FromJSON TestData where
   parseJSON = genericParseJSON $ defaultOptions { omitNothingFields = True }
