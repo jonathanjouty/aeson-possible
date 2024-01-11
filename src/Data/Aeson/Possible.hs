95cf618e925b562a601f6729b828779faf107469
Jonathan Jouty <me@jonathanjouty.com>
2024-01-11 13:55:21 +0000

    format with fourmolu default config

--- src/Data/Aeson/Possible.hs
+++ src/Data/Aeson/Possible.hs
@@ -1,48 +1,48 @@
-module Data.Aeson.Possible (Possible(..), fromPossible) where
+module Data.Aeson.Possible (Possible (..), fromPossible) where
 
 import Data.Aeson
 import GHC.Generics (Generic)
 
 data Possible a = Missing | HaveNull | HaveData a
-  deriving stock (Show, Generic, Functor)
+    deriving stock (Show, Generic, Functor)
 
-instance Eq a => Eq (Possible a) where
-  Missing    == Missing    = True
-  Missing    == _          = False
-  HaveNull   == HaveNull   = True
-  HaveNull   == _          = False
-  HaveData a == HaveData b = a == b
-  HaveData _ == _          = False
+instance (Eq a) => Eq (Possible a) where
+    Missing == Missing = True
+    Missing == _ = False
+    HaveNull == HaveNull = True
+    HaveNull == _ = False
+    HaveData a == HaveData b = a == b
+    HaveData _ == _ = False
 
-instance Semigroup a => Semigroup (Possible a) where
-  Missing    <> x = x
-  HaveNull   <> _ = HaveNull
-  HaveData a <> _ = HaveData a
+instance (Semigroup a) => Semigroup (Possible a) where
+    Missing <> x = x
+    HaveNull <> _ = HaveNull
+    HaveData a <> _ = HaveData a
 
-instance Monoid a => Monoid (Possible a) where
-  mempty = Missing
+instance (Monoid a) => Monoid (Possible a) where
+    mempty = Missing
 
 instance Applicative Possible where
     pure = HaveData
     (HaveData f) <*> (HaveData x) = HaveData (f x)
-    (HaveData _) <*> Missing      = Missing
-    Missing      <*> Missing      = Missing
-    Missing      <*> (HaveData _) = Missing
-    _            <*> _            = HaveNull
+    (HaveData _) <*> Missing = Missing
+    Missing <*> Missing = Missing
+    Missing <*> (HaveData _) = Missing
+    _ <*> _ = HaveNull
 
-instance ToJSON a => ToJSON (Possible a) where
-  toJSON     = toJSON . fromPossible
-  toEncoding = toEncoding . fromPossible
-  omitField Missing      = True
-  omitField HaveNull     = False
-  omitField (HaveData _) = False
+instance (ToJSON a) => ToJSON (Possible a) where
+    toJSON = toJSON . fromPossible
+    toEncoding = toEncoding . fromPossible
+    omitField Missing = True
+    omitField HaveNull = False
+    omitField (HaveData _) = False
 
-instance FromJSON a => FromJSON (Possible a) where
-  parseJSON Null = pure HaveNull
-  parseJSON v    = fmap pure . parseJSON $ v
-  omittedField = Just Missing
+instance (FromJSON a) => FromJSON (Possible a) where
+    parseJSON Null = pure HaveNull
+    parseJSON v = fmap pure . parseJSON $ v
+    omittedField = Just Missing
 
 fromPossible :: Possible a -> Maybe a
-fromPossible Missing      = Nothing
-fromPossible HaveNull     = Nothing
+fromPossible Missing = Nothing
+fromPossible HaveNull = Nothing
 fromPossible (HaveData a) = Just a
--- test/Main.hs
+++ test/Main.hs
@@ -1,15 +1,15 @@
 module Main (main) where
 
+import Test.QuickCheck.Gen as Gen
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck as QC
-import Test.QuickCheck.Gen as Gen
 import Test.Tasty.QuickCheck.Laws
 
 import Data.Aeson
-import Data.Typeable
 import Data.Aeson.Possible
 import Data.Monoid (Sum)
+import Data.Typeable
 import GHC.Generics (Generic)
 
 main :: IO ()
@@ -18,55 +18,62 @@ main = defaultMain tests
 tests :: TestTree
 tests = testGroup "Tests" [laws, unitTests]
 
-newtype A_Possible a = A_Possible { unwrap :: Possible a }
-  deriving stock (Show, Generic, Functor)
-  deriving newtype (Eq, Semigroup, Monoid, Applicative)
+newtype A_Possible a = A_Possible {unwrap :: Possible a}
+    deriving stock (Show, Generic, Functor)
+    deriving newtype (Eq, Semigroup, Monoid, Applicative)
 
-instance Arbitrary a => Arbitrary (A_Possible a) where
-  arbitrary = A_Possible <$> Gen.oneof
-    [pure Missing, pure HaveNull, HaveData <$> arbitrary]
-  shrink (A_Possible v) = A_Possible <$> genericShrink v
+instance (Arbitrary a) => Arbitrary (A_Possible a) where
+    arbitrary =
+        A_Possible
+            <$> Gen.oneof
+                [pure Missing, pure HaveNull, HaveData <$> arbitrary]
+    shrink (A_Possible v) = A_Possible <$> genericShrink v
 
 laws :: TestTree
-laws = testGroup "Laws"
-  [ testEqLaws (Proxy :: Proxy (A_Possible Bool))
-  , testMonoidLaws (Proxy :: Proxy (A_Possible (Sum Int)))
-  , testSemigroupLaws (Proxy :: Proxy (A_Possible (Sum Int)))
-  , testFunctorLaws
-      (Proxy :: Proxy (A_Possible))
-      (Proxy :: Proxy Int)
-      (Proxy :: Proxy Int)
-      (Proxy :: Proxy Int)
-      (Proxy :: Proxy Int)
-      (\_ fu1 fu2 -> fu1 == fu2)
-  , testApplicativeLaws
-      (Proxy :: Proxy (A_Possible))
-      (Proxy :: Proxy Int)
-      (Proxy :: Proxy Int)
-      (Proxy :: Proxy Int)
-      (Proxy :: Proxy Int)
-      (\_ fu1 fu2 -> fu1 == fu2)
-  ]
+laws =
+    testGroup
+        "Laws"
+        [ testEqLaws (Proxy :: Proxy (A_Possible Bool))
+        , testMonoidLaws (Proxy :: Proxy (A_Possible (Sum Int)))
+        , testSemigroupLaws (Proxy :: Proxy (A_Possible (Sum Int)))
+        , testFunctorLaws
+            (Proxy :: Proxy (A_Possible))
+            (Proxy :: Proxy Int)
+            (Proxy :: Proxy Int)
+            (Proxy :: Proxy Int)
+            (Proxy :: Proxy Int)
+            (\_ fu1 fu2 -> fu1 == fu2)
+        , testApplicativeLaws
+            (Proxy :: Proxy (A_Possible))
+            (Proxy :: Proxy Int)
+            (Proxy :: Proxy Int)
+            (Proxy :: Proxy Int)
+            (Proxy :: Proxy Int)
+            (\_ fu1 fu2 -> fu1 == fu2)
+        ]
 
 data TestData = TestData
-  { a :: Possible Bool
-  , b :: Possible Bool
-  , c :: Possible Bool
-  } deriving stock (Show, Generic, Eq)
+    { a :: Possible Bool
+    , b :: Possible Bool
+    , c :: Possible Bool
+    }
+    deriving stock (Show, Generic, Eq)
 
 instance FromJSON TestData where
-  parseJSON = genericParseJSON $ defaultOptions { omitNothingFields = True }
+    parseJSON = genericParseJSON $ defaultOptions{omitNothingFields = True}
 
 instance ToJSON TestData where
-  toJSON = genericToJSON $ defaultOptions { omitNothingFields = True }
+    toJSON = genericToJSON $ defaultOptions{omitNothingFields = True}
 
 unitTests :: TestTree
-unitTests = testGroup "Unit Tests"
-  [ testCase "Decoding works as expected" $
-      eitherDecode json @?= Right value
-  , testCase "Encoding works as expected" $
-      encode value @?= json
-  ]
+unitTests =
+    testGroup
+        "Unit Tests"
+        [ testCase "Decoding works as expected" $
+            eitherDecode json @?= Right value
+        , testCase "Encoding works as expected" $
+            encode value @?= json
+        ]
   where
     json = "{\"b\":null,\"c\":true}"
-    value = TestData { a = Missing, b = HaveNull, c = HaveData True }
+    value = TestData{a = Missing, b = HaveNull, c = HaveData True}
